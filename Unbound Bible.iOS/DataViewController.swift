f2066ae7188f88cad7a5a4bc2bca55fe9f93100d
vrybant <vrybant@gmail.com>
2018-04-25 11:11:22 +0300

    iOS

---
+++ Unbound Bible.iOS/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Unbound Bible.iOS
+//
+//  Created by Vladimir Rybant on 25.04.2018.
+//  Copyright © 2018 Vladimir Rybant. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
