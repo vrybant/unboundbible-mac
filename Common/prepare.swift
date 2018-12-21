9d3bfb09722aede2798e9ae878ed36868cff192f
unboundbible <vrybant@gmail.com>
2018-12-21 22:46:09 +0300

    tags

--- Common/prepare.swift
+++ Common/prepare.swift
@@ -7,59 +7,27 @@
 
 import Foundation
 
-private func replaceTag(_ string: inout String) {
-    var r = string
-
-    switch string {
-        case   "<J>" : r = "<FR>"
-        case  "</J>" : r = "<Fr>"
-        case   "<t>" : r = "<FO>" // quote
-        case  "</t>" : r = "<Fo>"
-        case   "<h>" : r = "<TS>" // title
-        case  "</h>" : r = "<Ts>"
-        case   "<f>" : r = "<RF>" // footnote
-        case  "</f>" : r = "<Rf>"
-        default : break
-    }
-    switch string.lowercased() {
-        case   "<i>" : r = "<FI>"
-        case  "</i>" : r = "<Fi>"
-        case  "<em>" : r = "<FI>"
-        case "</em>" : r = "<Fi>"
-        case "<pb/>" : r = ""     // unused tags
-        case "<br/>" : r = ""
-        default : break
-    }
-    string = r
-}
+private let dictionary = [
+      "<J>":"<FR>",
+     "</J>":"<Fr>",
+      "<t>":"<FO>", // quote
+     "</t>":"<Fo>",
+      "<h>":"<TS>", // title
+     "</h>":"<Ts>",
+      "<f>":"<RF>", // footnote
+     "</f>":"<Rf>",
+      "<I>":"<FI>",
+     "</I>":"<Fi>",
+      "<i>":"<FI>",
+     "</i>":"<Fi>",
+     "<em>":"<FI>",
+    "</em>":"<Fi>"]
 
 private func replaceTags(list: inout [String]) {
     for i in 0...list.count-1 {
-        replaceTag(&list[i])
+	  if let value = dictionary[list[i]] { list[i] = value }
     }
 }
- 
- /*
-function Prepare(s: string; format: TFileFormat; purge: boolean = true): string;
-var
-  List : TStringArray;
-begin
-  List := XmlToList(s);
-
-  if format = unbound then
-    begin
-      Strongs(List);
-      Footnotes(List);
-    end;
-
-  ReplaceTags(List);
-  PurgeTag(List,'<TS>','<Ts>');
-  if purge then PurgeTag(List, '<RF','<Rf>');
-
-  Result := Trim(ListToString(List));
-  Replace(Result,'</S><S>','</S> <S>');
-end;
-*/
 
 func prepare(_ string: String, format: FileFormat, purge: Bool = true)-> String {
     var list = xmlToList(string: string)
