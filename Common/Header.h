f62b4ed18a9009a94cb87a1fa9e95d7eb931af69
unboundbible <vrybant@gmail.com>
2018-12-29 11:44:39 +0300

    header

---
+++ Common/Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "FMDB.h"
+
+// #import <sqlite3.h>
+// #import "FMDatabase.h"
+// #import "FMResultSet.h"
+// #import "FMDatabaseAdditions.h"
