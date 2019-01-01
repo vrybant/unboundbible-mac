2511b5108ca1dbfa3fe9863dbf5808a2e358bac2
unboundbible <vrybant@gmail.com>
2019-01-01 18:10:54 +0300

    comments

--- Common/lib.swift
+++ Common/lib.swift
@@ -105,43 +105,6 @@ func orthodox(language: String) -> Bool {
     return false
 }
 
-//    func getFileListWithExt(_ path: String, ext: String) -> [String] {
-//     return getFileList(path).filter{ $0.hasSuffix(".\(ext)") }
-//    }
-
-//    func StringByDeletingPathExtension(_ path: String) -> String {
-//        return (NSURL(fileURLWithPath: path).deletingPathExtension?.lastPathComponent)! as String
-//    }
-
-//    func readFromFile(_ path: String) -> String {
-//        var text : String
-//        do {
-//            try text = NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue) as String
-//        }
-//        catch {
-//            return ""
-//        }
-//        return text
-//    }
-
-//    func writeToFile(_ path: String, value: String) -> Bool {
-//        do {
-//            try value.write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
-//        } catch {
-//            return false
-//        }
-//        return true
-//    }
-
-//    func showClipboardContent(_ sender: NSButton) {
-//        let pb = NSPasteboard.general
-//        for item in pb.pasteboardItems ?? [] {
-//            if let str = item.string(forType: NSPasteboard.PasteboardType.string) {
-//                print(str)
-//            }
-//        }
-//    }
-
 func copyToClipboard(content: String) {
     let Pasteboard = NSPasteboard.general
     Pasteboard.clearContents()
