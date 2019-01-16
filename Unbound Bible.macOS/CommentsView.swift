443d727da08138cffd072b26da9d1eba7c6aa190
unboundbible <vrybant@gmail.com>
2019-01-16 22:35:30 +0300

    CommentsView

---
+++ Unbound Bible.macOS/CommentsView.swift
@@ -0,0 +1,19 @@
+//
+//  CommentView.swift
+//  Unbound Bible
+//
+//  Copyright © 2019 Vladimir Rybant. All rights reserved.
+//
+
+import Cocoa
+
+var commentsView = CommentsView()
+
+class CommentsView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
