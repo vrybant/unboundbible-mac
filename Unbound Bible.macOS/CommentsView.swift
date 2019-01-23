f3733ca5647a38c8001cc09f44633e2eb9cbd2a5
unboundbible <vrybant@gmail.com>
2019-01-23 22:54:30 +0300

    CommentsView

--- Unbound Bible.macOS/CommentsView.swift
+++ Unbound Bible.macOS/CommentsView.swift
@@ -11,9 +11,26 @@ var commentsView = CommentsView()
 
 class CommentsView: NSViewController {
 
+    @IBOutlet weak var textView: NSTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        commentsView = self
     }
+    
+    override func viewDidAppear() {
+        super.viewWillAppear()
+        
+        self.view.window!.styleMask.remove(NSWindow.StyleMask.fullScreen)
+        
+        view.window?.standardWindowButton(.miniaturizeButton)?.isHidden = true
+        view.window?.standardWindowButton(.zoomButton       )?.isHidden = true
+        
+        if shelf.isEmpty { return }
+        
+        textView.baseWritingDirection = bible!.rightToLeft ? .rightToLeft : .leftToRight
+        textView.textStorage?.setAttributedString(copyVerses(options: []))
+    }
+
     
 }
