a904889c186462d748a7aee4ff7aee49eaf0944c
unboundbible <vrybant@gmail.com>
2019-09-08 15:23:35 +0300

    ids

--- Common/data.swift
+++ Common/data.swift
@@ -93,42 +93,23 @@ struct Content {
 }
 
 let myBibleArray : [Int] = [0,
-                            010,020,030,040,050,060,070,080,090,100,110,120,130,140,150,160,190,220,230,240,
-                            250,260,290,300,310,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,
-                            480,490,500,510,520,530,540,550,560,570,580,590,600,610,620,630,640,650,660,670,
-                            680,690,700,710,720,730,165,170,180,270,280,315,320,462,464,466,468,790,467]
-
-let sortArrayEN : [Int] = [0,
-                           01,02,03,04,05,06,07,08,09,10,11,12,13,14,78,15,16,67,68,69,
-                           17,74,75,76,79,77,18,19,20,21,22,70,71,23,24,25,72,73,26,27,
-                           28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
-                           48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66]
-
-let sortArrayRU : [Int] = [0,
-                           01,02,03,04,05,06,07,08,09,10,11,12,13,14,78,15,16,67,68,69,
-                           17,18,19,20,21,22,70,71,23,24,25,72,73,26,27,28,29,30,31,32,
-                           33,34,35,36,37,38,39,74,75,76,79,77,40,41,42,43,44,59,60,61,
-                           62,63,64,65,45,46,47,48,49,50,51,52,53,54,55,56,57,58,66]
+        010,020,030,040,050,060,070,080,090,100,110,120,130,140,150,160,190,220,230,240,
+        250,260,290,300,310,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,
+        480,490,500,510,520,530,540,550,560,570,580,590,600,610,620,630,640,650,660,670,
+        680,690,700,710,720,730,000,000,000,000,000,000,000,000,000,000,165,468,170,180,
+        462,464,466,467,270,280,315,320]
 
 let bibleHubArray : [String] = ["",
-                                "genesis","exodus","leviticus","numbers","deuteronomy","joshua","judges","ruth","1_samuel","2_samuel",
-                                "1_kings","2_kings","1_chronicles","2_chronicles","ezra","nehemiah","esther","job","psalms","proverbs",
-                                "ecclesiastes","songs","isaiah","jeremiah","lamentations","ezekiel","daniel","hosea","joel","amos",
-                                "obadiah","jonah","micah","nahum","habakkuk","zephaniah","haggai","zechariah","malachi","matthew",
-                                "mark","luke","john","acts","romans","1_corinthians","2_corinthians","galatians","ephesians","philippians",
-                                "colossians","1_thessalonians","2_thessalonians","1_timothy","2_timothy","titus","philemon","hebrews",
-                                "james","1_peter","2_peter","1_john","2_john","3_john","jude","revelation"]
+        "genesis","exodus","leviticus","numbers","deuteronomy","joshua","judges","ruth","1_samuel","2_samuel",
+        "1_kings","2_kings","1_chronicles","2_chronicles","ezra","nehemiah","esther","job","psalms","proverbs",
+        "ecclesiastes","songs","isaiah","jeremiah","lamentations","ezekiel","daniel","hosea","joel","amos",
+        "obadiah","jonah","micah","nahum","habakkuk","zephaniah","haggai","zechariah","malachi","matthew",
+        "mark","luke","john","acts","romans","1_corinthians","2_corinthians","galatians","ephesians","philippians",
+        "colossians","1_thessalonians","2_thessalonians","1_timothy","2_timothy","titus","philemon","hebrews",
+        "james","1_peter","2_peter","1_john","2_john","3_john","jude","revelation"]
 
 func isNewTestament(_ n: Int) -> Bool {
-    return (n >= 40) && (n <= 66)
-}
-
-func isOldTestament(_ n: Int) -> Bool {
-    return n < 40
-}
-
-func isApocrypha(_ n: Int) -> Bool {
-    return n > 66
+    return (n >= 40) && (n < 77)
 }
 
 var defaultBible: String {
