680f414526e758d6c5fab24ca8fc48ff1722ac9f
unboundbible <vrybant@gmail.com>
2019-12-19 11:25:29 +0300

    tabbed

---
+++ Unbound Bible.iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Unbound Bible.iOS
+//
+//  Created by Vladimir Rybant on 19.12.2019.
+//  Copyright © 2019 Vladimir Rybant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unbound Bible.iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Unbound Bible.iOS
+//
+//  Created by Vladimir Rybant on 19.12.2019.
+//  Copyright © 2019 Vladimir Rybant. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
