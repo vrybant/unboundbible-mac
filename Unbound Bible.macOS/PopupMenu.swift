c50e3ec8ac46f54241925aeb8c6193c55e7d5e0a
unboundbible <vrybant@gmail.com>
2020-07-15 20:57:00 +0300

    popup

--- Unbound Bible.macOS/PopupMenu.swift
+++ Unbound Bible.macOS/PopupMenu.swift
@@ -12,16 +12,25 @@ class PopupMenu: NSMenu {
     @IBOutlet weak var searchMenuItem: NSMenuItem!
     @IBOutlet weak var lookupMenuItem: NSMenuItem!
     @IBOutlet weak var copyMenuItem: NSMenuItem!
+    @IBOutlet weak var separatorMenuItem: NSMenuItem!
+    @IBOutlet weak var interlinearMenuItem: NSMenuItem!
     
     override func update() {
         self.autoenablesItems = false
  
-        let selectedString = rigthView.bibleTextView.selectedString.trimmed
-        let enabled = selectedString.length > 1
+        let selection = rigthView.bibleTextView.selectedString.trimmed
+        let enabled = (selection.length > 1) && !selection.contains("\n")
+
+        searchMenuItem.isHidden = !enabled
+        lookupMenuItem.isHidden = !enabled
+        separatorMenuItem.isHidden = !enabled
+        copyMenuItem.isEnabled = !selection.isEmpty
+        interlinearMenuItem.isHidden = selection.contains("\n")
         
-        searchMenuItem.isEnabled = enabled
-        lookupMenuItem.isEnabled = enabled
-        copyMenuItem.isEnabled   = !selectedString.isEmpty
+        if enabled {
+            searchMenuItem.title = LocalizedString("Search for %").replace("%", with: selection.quoted)
+            lookupMenuItem.title = LocalizedString("Look Up %")   .replace("%", with: selection.quoted)
+        }
     }
 
 }
