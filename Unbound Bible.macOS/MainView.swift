e415b7c927baba0e45b3a36d71e3e7b37f6c59c4
unboundbible <vrybant@gmail.com>
2021-10-15 00:07:08 +0300

    storyboards

--- Unbound Bible.macOS/MainView.swift
+++ Unbound Bible.macOS/MainView.swift
@@ -204,11 +204,12 @@ class MainView: NSViewController, NSWindowDelegate {
     }
 
     func welcome() {
-//      if !applicationUpdate { return }
+        if !applicationUpdate { return }
         if !russianSpeaking { return }
-        if let welcomeView = storyboard?.instantiateController(withIdentifier: "WelcomeView") as? WelcomeView {
+        let storyboard = NSStoryboard(name: "Additional", bundle: Bundle.main)
+        if let welcomeView = storyboard.instantiateController(withIdentifier: "WelcomeView") as? WelcomeView {
             presentAsModalWindow(welcomeView)
-        }
+      }
     }
     
 }
