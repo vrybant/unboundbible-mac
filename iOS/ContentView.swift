58b67573caaf1c03378faeadbb8f2f897ca18c83
unboundbible <vrybant@gmail.com>
2021-11-22 21:29:38 +0300

    SwiftUI

---
+++ iOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by Vladimir Rybant on 22/11/2021.
+//  Copyright © 2021 Vladimir Rybant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/iOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Vladimir Rybant on 22/11/2021.
+//  Copyright © 2021 Vladimir Rybant. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
