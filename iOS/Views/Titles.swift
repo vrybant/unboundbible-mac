149eb47e88ea12346d084e1cd3d721133d3d3070
Vladimir Rybant <vrybant@gmail.com>
2023-06-08 22:17:28 +0300

    update

--- iOS/Views/Titles.swift
+++ iOS/Views/Titles.swift
@@ -10,28 +10,18 @@ import SwiftUI
 struct TitlesView: View {
     @Environment(\.presentationMode) var presentation
 
-    struct Title: Identifiable {
-        let name: String
-        let id = UUID()
-    }
+    let titlesArray : [String] = [ // "",
+        "Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel",
+        "1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Esther","Job","Psalms","Proverbs",
+        "Ecclesiastes","Songs","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos",
+        "Obadiah","Jonah","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi","Matthew",
+        "Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians",
+        "Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews",
+        "James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"]
     
-    private var titles = [
-        Title(name: "Genesis"),
-        Title(name: "Exodus"),
-        Title(name: "Leviticus"),
-        Title(name: "Numbers"),
-        Title(name: "Deuteronomy"),
-        Title(name: "Joshua"),
-        Title(name: "Judges"),
-        Title(name: "Ruth"),
-        Title(name: "1 Samuel"),
-        Title(name: "2 Samuel")
-    ]
-
     public var body: some View {
-        List(titles) {
-            Text($0.name)
+        List(titlesArray, id: \.self) { item in
+            Text(item)            
         }
-//      .navigationBarBackButtonHidden(true)
     }
 }
