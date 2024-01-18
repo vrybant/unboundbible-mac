f4b4f00b3a97c4bc64a797b5d3e0b53ba530c4dc
Vladimir Rybant <vrybant@gmail.com>
2024-01-18 23:26:06 +0300

    update

--- SwiftUI/Views/ShelfView.swift
+++ SwiftUI/Views/ShelfView.swift
@@ -6,29 +6,34 @@
 import SwiftUI
 
 public struct ShelfView: View {
-    @State private var selection: String?
+    @State private var selection: String = currBible!.name
 
     public var body: some View {
         let list = tools.get_Shelf()
         
         NavigationStack {
-            List(list, id: \.self, selection: $selection) { item in
-                HStack {
-                    Text(item)
-                        .onTapGesture {
-                            print($selection)
-                        }
-                    Spacer()
-                    let checked = item == currBible!.name
-                    Image(systemName: checked ? "checkmark" : "")
+            VStack {
+                Text(selection)
+                List(list, id: \.self /*, selection: $selection */) { item in
+                    HStack {
+                        Text(item)
+                            .onTapGesture {
+                                selection = item
+                                tools.setCurrBible(item)
+                                print(selection)
+                            }
+                        Spacer()
+                        let checked = item == currBible!.name
+                        Image(systemName: checked ? "checkmark" : "")
+                    }
+                    
                 }
-
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("Modules")
+                //          .toolbar {
+                //              EditButton()
+                //          }
             }
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationTitle("Modules")
-//          .toolbar {
-//              EditButton()
-//          }
         }
     }
 }
