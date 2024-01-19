bbbbf354ee9a9a42b1f8d79eb3807498b54dac18
Vladimir Rybant <vrybant@gmail.com>
2024-01-19 17:09:16 +0300

    search

--- SwiftUI/Views/SearchView.swift
+++ SwiftUI/Views/SearchView.swift
@@ -3,6 +3,8 @@
 //  Copyright © 2024 Vladimir Rybant. All rights reserved.
 //
 
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-a-search-bar-to-filter-your-data
+
 import SwiftUI
 
 struct Food: Identifiable {
@@ -13,10 +15,7 @@ struct Food: Identifiable {
 
     static func preview() -> [Food] {
         return [Food(name: "Apple", icon: "🍎", isFavorite: true),
-                Food(name: "Banana", icon: "🍌", isFavorite: false),
                 Food(name: "Cherry", icon: "🍒", isFavorite: false),
-                Food(name: "Mango", icon: "🥭", isFavorite: true),
-                Food(name: "Kiwi", icon: "🥝", isFavorite: false),
                 Food(name: "Strawberry", icon: "🍓", isFavorite: false),
                 Food(name: "Graps", icon: "🍇", isFavorite: true)
         ]
@@ -25,15 +24,29 @@ struct Food: Identifiable {
 
 struct SearchView: View {
     @State private var foods = Food.preview()
+    @State private var searchText = ""
+    @State private var searchIsActive = false
 
     var body: some View {
-        List (foods) { food in
-//          FoodRow(food: food)
-            HStack {
-                Text(food.icon)
-                Text(food.name)
-                Spacer()
-                Image(systemName: food.isFavorite ? "heart.fill" : "heart")
+        VStack {
+            NavigationStack {
+                List (foods) { food in
+                    HStack {
+                        Text(food.icon)
+                        Text(food.name)
+                        Spacer()
+                        Image(systemName: food.isFavorite ? "heart.fill" : "heart")
+                    }
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("Search")
+            }
+            .searchable(text: $searchText, isPresented: $searchIsActive, prompt: "Search text")
+            .onChange(of: searchText) {
+                print(searchText)
+            }
+            .onSubmit {
+                print(searchText)
             }
         }
     }
