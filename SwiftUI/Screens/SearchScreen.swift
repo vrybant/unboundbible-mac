9ab9238d19dd6889a3e9045d771b154fe0cbf073
Vladimir Rybant <vrybant@gmail.com>
2025-03-10 18:11:49 +0300

    update

--- SwiftUI/Screens/SearchScreen.swift
+++ SwiftUI/Screens/SearchScreen.swift
@@ -3,21 +3,31 @@
 //  Copyright © Vladimir Rybant.
 //
 
-// https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-a-search-bar-to-filter-your-data
 // https://www.hackingwithswift.com/quick-start/swiftui/how-to-run-some-code-when-state-changes-using-onchange
 
 import SwiftUI
 
 struct SearchScreen: View {
     @State var store = SearchStore.shared
+    @State var selection: String? = nil
 
+    func onTap(_ item: String) {
+        selection = item
+//      BibleStore.shared.update(book: book!, chapter: chapter)
+//      BibleStore.shared.router.removeAll()
+        TabsStore.shared.selection = .bible
+    }
+    
     var body: some View {
         VStack {
             NavigationStack {
-                List (store.content, id: \.self) { item in
+                List(store.content, id: \.self, selection: $selection) { item in
                     let attrString = parse(item)
                     let content = AttributedString(attrString)
                     Text(content)
+                        .onTapGesture {
+                            onTap(item)
+                        }
                 }
                 .navigationTitle("Search")
                 .safeNavigationBarTitleDisplayMode(.inline)
