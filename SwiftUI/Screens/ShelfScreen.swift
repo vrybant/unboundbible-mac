bbf2c74a867b5a6aeff4fc95ffbe9886951b6089
Vladimir Rybant <vrybant@gmail.com>
2025-03-14 19:57:49 +0300

    update

--- SwiftUI/Screens/ShelfScreen.swift
+++ SwiftUI/Screens/ShelfScreen.swift
@@ -6,27 +6,33 @@
 import SwiftUI
 
 public struct ShelfScreen: View {
+    @State var selection: UUID? = nil
+    
     var store = ShelfStore.shared
     let content = ShelfStore.shared.content
     
     public var body: some View {
         NavigationStack {
             VStack {
-                List(content) { item in
+                List(content, selection: $selection) { item in
                     HStack {
                         Text(item.string)
                             .frame(maxWidth: .infinity, alignment: .leading)
-                            .contentShape(Rectangle())
-                            .onTapGesture {
-                                store.update(bible: item.string)
-                            }
                         Spacer()
                         Image(systemName: "checkmark")
                             .opacity(store.isCurrent(name: item.string) ? 1.0 : 0.0)
                     }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        selection = item.id
+                        store.update(bible: item.string)
+                    }
                 }
                 .padding(.top, -20)
                 .navigationTitle("Modules")
+//              .onAppear {
+//                  selection = content.first // Set default selection
+//              }
                 .safeNavigationBarTitleDisplayMode(.inline)
             }
         }
