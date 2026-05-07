c5fe8abca69c3404d16d0b32ec035c44e8cb2851
Vladimir Rybant <vrybant@gmail.com>
2026-05-07 11:26:58 +0300

    get

--- macOS/Views/ToolsExt.swift
+++ macOS/Views/ToolsExt.swift
@@ -6,21 +6,15 @@
 extension Tools {
     
        func get_Chapter(book: Int, chapter: Int) -> [String] {
-           var result = [RowData]()
+           var result = [String]()
            let rowData = currBible.getChapter(book: book, chapter: chapter)
-           
-           let space = cocoaApp ? " " : ""
-           let dot = cocoaApp ? "" : "."
-           let eol = cocoaApp ? "\n" : ""
 
            for item in rowData {
-               var item = item
-               let text = "\(space)<l>\(item.number)\(dot)</l> \(item.text)\(eol)"
-               item.text = text
-               result.append(item)
+               let text = " <l>\(item.number)</l> \(item.text)\n"
+               result.append(text)
            }
            
-           return result.text
+           return result
        }
     
 }
