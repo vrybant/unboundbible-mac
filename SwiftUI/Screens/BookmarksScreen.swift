2d73a23deb05b44ca10501aa726bbea30df42524
Vladimir Rybant <vrybant@gmail.com>
2026-05-12 20:44:03 +0300

    bookmark

--- SwiftUI/Screens/BookmarksScreen.swift
+++ SwiftUI/Screens/BookmarksScreen.swift
@@ -6,13 +6,32 @@
 import SwiftUI
 
 struct BookmarksScreen: View {
-    var bookmarksModel = BookmarksModel.shared
+    @State var bookmarksModel = BookmarksModel.shared
+    @State var selection: UUID? = nil
         
+    func onTap(_ item: RowData) {
+        let verse = item.verse
+        if currBible.goodLink(verse) {
+            BibleModel.shared.update(book: verse.book, chapter: verse.chapter)
+            BibleModel.shared.router.removeAll()
+            HomeModel.shared.selection = .bible
+        }
+    }
+    
     var body: some View {
         NavigationStack {
-            List(bookmarksModel.content,  id: \.id) { item in
+            List(bookmarksModel.content,  id: \.id, selection: $selection) { item in
                 let attrString = parse(item.text)
-                Text(attrString)
+                let link = currBible.verseToString(item.verse) ?? "---"
+                VStack(alignment: .leading) {
+                    Text(attrString)
+                    Text(link)
+                        .foregroundColor(.gray)
+                }
+                .onTapGesture {
+                    selection = item.id
+                    onTap(item)
+                }
             }
             .padding(.top, -20)
             .navigationTitle("Bookmarks")
