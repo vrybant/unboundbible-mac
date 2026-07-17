e1d62fb5c98285bf3e5683bb46bf68bd1553edcf
Vladimir Rybant <vrybant@gmail.com>
2026-07-17 20:58:55 +0300

    update

--- SwiftUI/Models/BookmarksModel.swift
+++ SwiftUI/Models/BookmarksModel.swift
@@ -7,12 +7,8 @@ import Foundation
 
 @Observable class BookmarksModel {
     static let shared = BookmarksModel()
-    var content: [RowData]
+    var content: [RowData] = UserDefaults.standard.rowData(forKey: "bookmarks")
     
-    private init() {
-        content = UserDefaults.standard.rowData(forKey: "bookmarks")
-    }
-
     func update(_ bookmark: RowData) {
         content.append(bookmark)
         UserDefaults.standard.set(content, forKey: "bookmarks")
